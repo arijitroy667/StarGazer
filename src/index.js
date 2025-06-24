bf36b0b7032f9c8e3a982e7ca5c876fb21d0ad2f
arijitroy667 <arijitroy0445@gmail.com>
2025-06-24 09:01:20 +0530

    commit
    utils files added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 //require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed:", err);
+  });
 
 // THESE LINES ARE USED WHEN CONNECTING MONGODB DIRECTLY
 /*
