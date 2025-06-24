8ada05182b3e4449d8a01d3d306e3b39753b8d0c
arijitroy667 <arijitroy0445@gmail.com>
2025-06-24 18:03:49 +0530

    commit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -9,7 +9,7 @@ class ApiError extends Error {
     this.statusCode = statuscode;
     this.data = null;
     this.message = message;
-    this.sucess = false;
+    this.success = false;
     this.errors = errors;
 
     if (stack) {
