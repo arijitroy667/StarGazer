333f72f932c5a5139d6c3192bad0c84c0f470081
arijitroy667 <arijitroy0445@gmail.com>
2025-06-25 17:38:14 +0530

    Controllers basics

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ app.use(express.urlencoded({ extented: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
 
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
