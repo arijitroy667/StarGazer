58401a7df7276448c2760a540771abd89202231e
arijitroy667 <arijitroy0445@gmail.com>
2025-07-03 00:26:52 +0530

    rest all models added

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      required: true,
+    },
+    tweet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tweet",
+      required: true,
+    },
+    likeBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
