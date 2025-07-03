c6d0a70106ea0e8a5d2332ba14ed83f6f5c5abf6
arijitroy667 <arijitroy0445@gmail.com>
2025-07-03 17:05:23 +0530

    commit

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  const videoComment = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videoDetails",
+      },
+    },
+    {
+      $unwind: "$videoDetails",
+    },
+  ]);
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
