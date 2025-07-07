9291f4201caec30a06c7d1a21692785f08da2600
arijitroy667 <arijitroy0445@gmail.com>
2025-07-07 09:10:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //require("dotenv").config();
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
 
 connectDB()
   .then(() => {
