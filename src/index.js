b6f715bf8478a0b9c5a4c58f7ff6a72bdbe44ee2
arijitroy667 <arijitroy0445@gmail.com>
2025-07-07 11:12:22 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //require("dotenv").config();
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./src/db/index.js";
-import { app } from "./src/app.js";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
   .then(() => {
