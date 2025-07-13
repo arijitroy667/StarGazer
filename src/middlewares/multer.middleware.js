bedced6e659b1e22f33d39aa410ac31b215d46da
ARIJIT ROY <72807485+arijitroy667@users.noreply.github.com>
2025-07-13 12:26:55 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -16,6 +16,10 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Ensure upload directory exists
 const uploadDir = path.resolve(__dirname, "../public/temp");
